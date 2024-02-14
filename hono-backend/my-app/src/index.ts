c1e1728f94ba267dee613587f79f2e1b8f414b2d
Shubham Sonthalia <shubhamsonthalia@Shubhams-MacBook-Air.local>
2024-02-15 01:34:27 +0530

    removing hono

--- hono-backend/my-app/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import { mainRouter } from "./routes/index";
-import cors from "cors";
-
-const app = new Hono();
-
-app.route("/api/v1", mainRouter);
-export default app;
